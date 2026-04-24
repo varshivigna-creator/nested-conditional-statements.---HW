b5dd77ba2a72b34b82217413bd8b331cf81b0401
varshivigna-creator <varshivigna@gmail.com>
2026-04-24 17:51:13 +0530

    age

--- N.cs .py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-age = int(input("enter your age: ")) 
-if age >= 10 and age < 20:         
-    print("age is between 10 & 20")
-else:
-    print("age is not between 10 & 20")
---
+++ ncs.py
@@ -0,0 +1,6 @@
+age = int(input("enter your age: ")) # 1. Convert input to an integer
+if age >= 10 and age < 20:           # 2. Repeat the variable name after 'and'
+    print("age is between 10 & 20")
+
+else :
+    print("no age is not between them")
